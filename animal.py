a11c847aa2d18ec0db1f245526918c3abc87f99a
Yasodha <yasodhavirajani@gmail.com>
2024-02-16 15:30:45 +0530

    resolved all merge conflicts

--- animal.py
+++ animal.py
@@ -1,10 +1,15 @@
 import sys
 
+def dog():
+    print('Baw')
 def default():
     print("Hello")
 
 def main():
-    default()
+    if sys.argv[1] == 'dog':
+        dog()
+    else:
+      default()
 
 if __name__ =='__main__':
     main()
